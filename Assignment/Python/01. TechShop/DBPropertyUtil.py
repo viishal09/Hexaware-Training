c6f1a83784bc41169b9df10b2649bf6ed3176157
Vishal Jha <87255898+viishal09@users.noreply.github.com>
2023-12-26 18:26:34 +0530

    Add files via upload

---
+++ Assignment/Python/01. TechShop/DBPropertyUtil.py
@@ -0,0 +1,9 @@
+class PropertyUtil:
+    @staticmethod
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = 'root'
+        database = 'techshop'
+        return host, username, password, database
+
